1f331e2707a19acd5eb547203829d2f84015f039
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-12 18:22:45 -0600

    Wrote list method for PaymentTypeView then tested in Postman. Got expected response.

--- galleryapi/views/payment_type.py
+++ galleryapi/views/payment_type.py
@@ -9,17 +9,29 @@ class PaymentTypeView(ViewSet):
     '''The Gallery's Payment Type View'''
     
     def retrieve(self, request, pk):
-      """Handle GET requests from single payment type
-      Returns:
-          Response -- JSON serialized payment type
-      """
-      try:
-          payment_type = PaymentType.objects.get(pk=pk)
+        """Handle GET requests from single payment type
+        Returns:
+            Response -- JSON serialized payment type
+        """
+        try:
+            payment_type = PaymentType.objects.get(pk=pk)
 
-          serializer = PaymentTypeSerializer(payment_type)
-          return Response(serializer.data)
-      except PaymentType.DoesNotExist as ex:
-          return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+            serializer = PaymentTypeSerializer(payment_type)
+            return Response(serializer.data)
+        except PaymentType.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    
+    def list(self, request):
+        """Handle GET requests to get all payment types
+
+        Returns:
+            Response -- JSON serialized list of payment types
+        """
+        payment_types = PaymentType.objects.all()
+            
+        serializer = PaymentTypeSerializer(payment_types, many=True)
+        
+        return Response(serializer.data)
         
 class PaymentTypeSerializer(serializers.ModelSerializer):
     '''JSON serializer for payment types'''
