b4de19fc50242f40a3c588a4d5750f327dc999b1
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-12 20:43:56 -0600

    Added create method to OrderView. Tested in Postman and getting a good response. Decided to put a pin in this branch to work on product_on_order view, so that when I test orders, I will also see the products associated with that order

--- galleryapi/views/order.py
+++ galleryapi/views/order.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import serializers, status
-from galleryapi.models import Order
+from galleryapi.models import Order, User, PaymentType
 
 class OrderView(ViewSet):
     '''The Gallery's Order View'''
@@ -22,6 +22,25 @@ class OrderView(ViewSet):
             orders = orders.filter(is_closed=True)
             
         serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
+      
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized order instance
+        """
+        customer = User.objects.get(uid=request.data['customer_id'])
+        payment_type = PaymentType.objects.get(pk=request.data['payment_type_id'])
+        
+        order = Order.objects.create(
+            total_cost=request.data['total_cost'],
+            is_closed=True,
+            customer=customer,
+            payment_type=payment_type
+        )
+        
+        serializer = OrderSerializer(order)
         return Response(serializer.data)
       
 class OrderSerializer(serializers.ModelSerializer):
