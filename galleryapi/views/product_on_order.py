59736815ba65fd4b040664ca35a6dca6101476c0
Sariah Campopiano <scampopiano925@gmail.com>
2023-01-23 21:45:22 -0600

    Added two query params for getting the list of productOnList which will be viewed in the cart. The params are user and if the order_id is null meaning the order hasn't been created

--- galleryapi/views/product_on_order.py
+++ galleryapi/views/product_on_order.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from galleryapi.models import ProductOnOrder, Product, User
+from galleryapi.models import ProductOnOrder
 from .order import OrderSerializer
 
 class ProductOnOrderView(ViewSet):
@@ -14,7 +14,19 @@ class ProductOnOrderView(ViewSet):
             Response -- JSON serialized list of events
         """
         products_on_order = ProductOnOrder.objects.all()
-    
+        
+        user_product_list = request.query_params.get('user', None)
+        if user_product_list is not None:
+            products_on_order = products_on_order.filter(user_id=user_product_list)
+    # Product_on_order with an order_id of NULL are the list of products in the user's cart
+        null_on_order = request.query_params.get('order', None)
+        if null_on_order is not None:
+            if null_on_order == "null":
+                null_on_order = None
+            else:
+                null_on_order = "didn't work"
+            products_on_order = products_on_order.filter(order_id=null_on_order)
+        
         serializer = ProductOnOrderSerializer(products_on_order, many=True)
         return Response(serializer.data)
 class ProductOnOrderSerializer(serializers.ModelSerializer):
